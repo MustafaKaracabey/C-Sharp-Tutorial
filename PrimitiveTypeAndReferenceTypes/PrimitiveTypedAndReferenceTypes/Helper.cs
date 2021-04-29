a867d30947c34fc0f3bf622897ac1447716510ab
Mustafa Karacabey <mustafa.karacabey@hepsiburada.com>
2021-04-29 23:28:25 +0300

    [C-Sharp-01]
    Worked on Reference Type and Value Type

---
+++ PrimitiveTypeAndReferenceTypes/PrimitiveTypedAndReferenceTypes/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypedAndReferenceTypes
+{
+    public class Helper
+    {
+
+        public static void Inrecement(int number3)
+        {
+            number3 += 10;
+            Console.WriteLine($"Your Number is {number3} ");
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
+
+    }
+}
---
+++ PrimitiveTypeAndReferenceTypes/PrimitiveTypedAndReferenceTypes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PrimitiveTypedAndReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CASE 1:
+
+            var number1 = 10;
+            var number2 = number1;
+            number2++;
+
+            Console.WriteLine($"number1 is : {number1} and number2 is: {number2}"); //Primitive Type
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            array2[1] = 5;
+            Console.WriteLine($"second element of array2 is {array2[1]}");
+            Console.WriteLine($"second element of array1 is {array1[1]}");
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+
+            //Case 2:
+
+            var number3 = 25;
+            Helper.Inrecement(number3);
+            Console.WriteLine($"number3 is : {number3}");
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            var person1 = new Person() { Age = 20 };
+            Helper.MakeOld(person1);
+            Console.WriteLine($"Person1 age is : {person1.Age}");
+
+           
+
+
+        }
+
+     
+
+    }
+}
