eb5fae4ff7c97d4c47e400ca7c49e891a46077d0
Mustafa Karacabey <mustafa.karacabey@hepsiburada.com>
2021-04-30 01:52:12 +0300

    [C-Sharp-04]
    I Worked on If-Else-Else-If

---
+++ If-Else/Comparison of 3 numbers/Comparison of 3 numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Comparison_of_3_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.WriteLine("Enter number a :");
+            a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter number b :");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number c :");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if( a >= b && a >= c )
+            {
+                Console.WriteLine("a is the biggest");
+            }
+            else if( b >= a && b >= c )
+            {
+                Console.WriteLine("b is the biggest");
+            }
+            else if( c >= a && c >= b )
+            {
+                Console.WriteLine("c is the biggest");
+            }
+            else
+            {
+                Console.WriteLine("All numbers are equal");
+            }
+
+
+        }
+    }
+}
