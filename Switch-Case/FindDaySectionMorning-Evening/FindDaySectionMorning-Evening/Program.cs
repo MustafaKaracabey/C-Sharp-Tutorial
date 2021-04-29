4cd46da9e25a54473f3764753c6bb322ae56b2be
Mustafa Karacabey <mustafa.karacabey@hepsiburada.com>
2021-04-30 02:27:00 +0300

    [C-Sharp-05]
    I Worked On Switch-Case

---
+++ Switch-Case/FindDaySectionMorning-Evening/FindDaySectionMorning-Evening/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FindDaySectionMorning_Evening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Time:");
+            int time = Convert.ToInt32(Console.ReadLine());
+
+            switch (time)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    Console.WriteLine("Night");
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                    Console.WriteLine("Morning");
+                    break;
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                    Console.WriteLine("noon");
+                    break;
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                case 24:
+                    Console.WriteLine("Evening");
+                    break;
+                default:
+                    Console.WriteLine("Wrong Number");
+                    break;
+            }
+        }
+    }
+}
