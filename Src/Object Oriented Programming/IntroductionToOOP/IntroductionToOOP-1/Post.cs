860b534248957fc91d2fa2a6098e0be91a3c1bc7
Mustafa Karacabey <mustafa.karacabey@hepsiburada.com>
2021-05-01 00:07:12 +0300

    [C-Sharp-06]
    I Worked Creating a  simple object

---
+++ Src/Object Oriented Programming/IntroductionToOOP/IntroductionToOOP-1/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToOOP
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Author { get; set; }
+
+
+        public void ShowPostProperties(Post post)
+        {
+            Console.WriteLine($" Author: {post.Author} \n Title: {post.Title} \n Image: {post.ImageUrl} \n Description: {post.Description} \n DateTime: {post.PublishDate}");
+        }
+    }
+}
---
+++ Src/Object Oriented Programming/IntroductionToOOP/IntroductionToOOP-1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IntroductionToOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post(); 
+
+            post1.Author = "Mustafa Karacabey";
+            post1.Description = "This Edcutaion Series is going to be perfect";
+            post1.PublishDate = DateTime.Now;
+            post1.ImageUrl = "https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2205232?start=15#overview";
+            post1.Title = "C-Sharp-Tutorial";
+
+            //post1.ShowPostProperties(post1);
+
+
+            var post2 = new Post();
+
+            post2.Author = "ZeynepKadir Karacabey";
+            post2.Description = "This Edcutaion Series is going to be perfect";
+            post2.PublishDate = DateTime.Now;
+            post2.ImageUrl = "https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2205232?start=15#overview";
+            post2.Title = "C-Sharp-Tutorial";
+
+            //post2.ShowPostProperties(post1);
+
+            Post[] Postlar = new Post[2] { post1, post2 };
+
+            foreach (var post in Postlar)
+            {
+                Console.WriteLine(post.PublishDate+post.Description+post.ImageUrl+post.Title+post.Author);
+                Console.WriteLine("------------------------------------");
+            }
+        }
+    }
+}
