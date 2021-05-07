66be89a6ac52fbb11657b8aaaa131c0316607ee6
Mustafa Karacabey <mustafa.karacabey@hepsiburada.com>
2021-05-07 23:49:13 +0300

    [C-Sharp-11]
    I Worked on Ref Modifier

---
+++ Src/Basic/RefModifier/RefModifier/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefModifier
+{
+    public static class Number
+    {
+        public static void ChangeByRefIncrement(ref int num)
+        {
+            num += 1;
+        }
+    }
+}
---
+++ Src/Basic/RefModifier/RefModifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RefModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine("Before Changing NUMBER is : " + number);
+
+            Number.ChangeByRefIncrement(ref number);//We can pass number like reference type even if value type
+            
+            Console.WriteLine("After Changing new NUMBER İS : " + number);
+        }
+    }
+}
