da351c5a4e0c996614ad4ac4419f390ac5a3cb24
Mustafa Karacabey <mustafa.karacabey@hepsiburada.com>
2021-05-07 23:34:27 +0300

    [C-Sharp-09]
    I Worked Constructor Method

---
+++ Src/Object Oriented Programming/Constructor/Constructor-1/Constructor-1/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_1
+{
+    public class Order
+    {
+        public string OrderName { get; set; }
+
+        public Order(string orderName)
+        {
+            this.OrderName = orderName;
+        }
+    }
+}
