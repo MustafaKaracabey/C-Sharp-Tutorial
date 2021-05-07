fedf9ca7698527c2cef8d358e809b18850eda6a9
Mustafa Karacabey <mustafa.karacabey@hepsiburada.com>
2021-05-08 00:08:29 +0300

    [C-Sharp-12]
    I Worked on Out Modifier

---
+++ Src/Basic/Out Modifier/Out Modifier/Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_Modifier
+{
+    public static class Number
+    {
+
+        //Out Keyword can update multiple parameter otherwise,ref keyword Can not
+        // No need to initialize variable if out keyword is used.otherwise,ref you have to Initialize   
+        public static void ChangeNumberByOut(out int number, out int number2)
+        {
+            number = 1;
+            number2 = 1;
+        }
+    }
+}
---
+++ Src/Basic/Out Modifier/Out Modifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Out_Modifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int number2 = 10;
+            Console.WriteLine("Before Changing NUMBER1 İS :" + number + " Before Changing NUMBER2 İS :" + number2);
+
+            Number.ChangeNumberByOut(out number,out number2);
+            Console.WriteLine("After Changing NUMBER1 İS :" + number + " After Changing NUMBER2 İS :"+ number2);
+        }
+    }
+}
