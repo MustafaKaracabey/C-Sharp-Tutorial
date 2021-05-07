6a255f1cbdd1363720ceee0d8f69976e0cd860e0
Mustafa Karacabey <mustafa.karacabey@hepsiburada.com>
2021-05-07 23:23:29 +0300

    [C-Sharp-08]
    I Worked on what the Constructor is

---
+++ Src/Object Oriented Programming/Constructor/Constructor-1/Constructor-1/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_1
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set;}
+
+        //Overloding : means having a method by the same name but different signatures
+        public Customer() // Parameterless Constructor
+        {
+            this.Orders = new List<Order>();
+        }
+        public Customer(string name)
+            : this() //Refers Parameterless Constructor 
+        {
+            
+            this.Name = name;
+        }
+        public Customer(string name, int id)
+            : this(name)
+        {
+            this.CustomerId = id;
+        }
+    }
+}
---
+++ Src/Object Oriented Programming/Constructor/Constructor-1/Constructor-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Constructor_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer("Mustafa", 1);
+
+            var pizza = new Order("Pizza");
+            var hamburger = new Order("Hamburger");
+
+            customer.Orders.Add(pizza);
+            customer.Orders.Add(hamburger);
+
+            Helper.ShowCustomerOrderList(customer);
+        }
+    }
+}
