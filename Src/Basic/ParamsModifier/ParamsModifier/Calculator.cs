8fe327205f6fce9c3c5ee6272d0610c836e30ac3
Mustafa Karacabey <mustafa.karacabey@hepsiburada.com>
2021-05-07 23:44:10 +0300

    [C-Sharp-10]
    I Worked on Params Modifier

---
+++ Src/Basic/ParamsModifier/ParamsModifier/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsModifier
+{
+    public class Calculator
+    {
+        public static int CalculateSum(params int[] numbers) // With Params We can pass array
+        {
+            var sum = numbers.Sum();
+            return sum;
+        }
+    }
+}
---
+++ Src/Basic/ParamsModifier/ParamsModifier/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ParamsModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = Calculator.CalculateSum(new int[] { 1, 2, 3, 4, 5, 6, 78, 9, 12 });
+            Console.WriteLine("Your Sum is : "+sum);
+        }
+    }
+}
