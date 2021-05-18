3a5ff054474693bc0e95260842c0f8833f151198
Mustafa Karacabey <mustafa.karacabey@outlook.com>
2021-05-18 23:20:12 +0300

    [C-Sharp-15]
    I Worked on AccessModifiers

---
+++ Src/Basic/AccesModifiers/AccesModifiers/ExplanationClass.cs
@@ -0,0 +1,19 @@
+﻿namespace AccesModifiers
+{
+    public class ExplanationClass
+    {
+        /*
+         *                          public  protected   internal    protected internal  private     private protected       
+         * entire program             Y         N          N              N                N                N
+         *Containing Class            Y         Y          Y              Y                Y                Y
+         *Currently Assembly          Y         N          Y              Y                N                 N
+         *Derived Types               Y         Y          N              Y                N                N
+    *Derived Types with currentassmbly Y        Y          Y              Y                N                Y
+         * 
+         *
+         *
+         *https://www.geeksforgeeks.org/access-modifiers-in-c-sharp/   (Reference)
+         * 
+         */
+    }
+}
