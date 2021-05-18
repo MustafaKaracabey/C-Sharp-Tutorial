2ef797863d802d5186207218e0abcab5ce039020
Mustafa Karacabey <mustafa.karacabey@outlook.com>
2021-05-19 00:06:25 +0300

    [C-Sharp-16]
    I Worked on Encapsulation Basic explanation

---
+++ Src/Object Oriented Programming/Encapsulation/Encapsulation-1/Encapsulation-1/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Encapsulation_1
+{
+    public class Person
+    { //Encapsulation Provides us to Safety.
+        private DateTime _birthdate;
+
+
+        public void SetBirthDate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthday()
+        {
+            return _birthdate;
+        }
+        
+    }
+}
---
+++ Src/Object Oriented Programming/Encapsulation/Encapsulation-1/Encapsulation-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Encapsulation_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            
+            person.SetBirthDate(DateTime.Now);
+
+            Console.WriteLine("My BirthDATE İS : " + person.GetBirthday());
+        }
+    }
+}
