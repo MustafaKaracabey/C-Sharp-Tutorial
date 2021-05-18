8568e82ed6171cd7db2b04645f09e7c6d3c562ae
Mustafa Karacabey <mustafa.karacabey@outlook.com>
2021-05-19 00:57:18 +0300

    [C-Sharp-19]
    I Worked and explain Composition vs Inheritance

---
+++ Src/Object Oriented Programming/Composition/Composition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Composition Like Dependency-Injection :)  -Code reuse, Flexibility, Loose-coupling -- Composition like Has Relationship
+            // In mY opinion Composition > Inheritance
+            // Inheritance (IS-A)
+            // Composition (Has-A)
+        }
+    }
+}
