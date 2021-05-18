3805e849e95755d15ce97aaa2d67f04981f3e14d
Mustafa Karacabey <mustafa.karacabey@outlook.com>
2021-05-19 00:46:58 +0300

    [C-Sharp-18]
    Introduction To Inheritance

---
+++ Src/Object Oriented Programming/Inheritance/Inheritance-1/Inheritance-1/PresentationObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inheritance_1
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        
+        public void Copy()
+        {
+            Console.WriteLine("Object Copied To Clipboard");
+        }
+
+        public void Duplicate()
+        {
+            Console.WriteLine("Object was duplicated");
+        } 
+    }
+}
---
+++ Src/Object Oriented Programming/Inheritance/Inheritance-1/Inheritance-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inheritance_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Copy();
+                
+        }
+    }
+}
