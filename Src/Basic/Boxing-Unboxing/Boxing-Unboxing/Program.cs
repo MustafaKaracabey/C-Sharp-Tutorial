3b64f7f68e71c16bbedafdc7f591f615905ed71d
Mustafa Karacabey <mustafa.karacabey@outlook.com>
2021-05-20 22:47:26 +0300

    [C-Sharp-22]
    What is the Boxing and Unboxing

---
+++ Src/Basic/Boxing-Unboxing/Boxing-Unboxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boxing mean You can convert Primitive type to Reference type
+
+            int number = 5;
+            Object obj = number; //This is boxing -- Other direction is Unboxing 
+            Console.WriteLine(obj.GetType());
+
+            
+                
+    
+
+        }
+    }
+}
